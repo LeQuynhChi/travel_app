3b9789100d600cecd029844bf325bd4200b459d4
PhamNgocBinh <teamgf2002@gmail.com>
2023-12-10 22:47:28 +0700

    add location

--- app/src/main/java/com/example/travelapp/network/RestApiService.java
+++ app/src/main/java/com/example/travelapp/network/RestApiService.java
@@ -3,9 +3,14 @@ package com.example.travelapp.network;
 import com.example.travelapp.model.Destination;
 import com.example.travelapp.model.Hotel;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -17,6 +22,16 @@ public interface RestApiService {
     Call<Destination.Data> getDestinationById(@Path("id") String id);
     @GET("hotel/all")
     Call<Hotel> getAllHotel();
-    @GET("destination/all")
-    Call<Destination> getProvinceVn();
+
+
+    @Multipart
+    @POST("destination/create")
+    Call<Destination.Data> createDestination(
+            @Part("name") RequestBody name,
+            @Part("description") RequestBody description,
+            @Part("province") RequestBody province,
+            @Part("district") RequestBody district,
+            @Part("type") RequestBody type,
+            @Part MultipartBody.Part image
+    );
 }
